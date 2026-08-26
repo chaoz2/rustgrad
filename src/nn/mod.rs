70c85551563b21aff4536d0a6ca828530bf41718
Chao Zhao <chaoz.db@icloud.com>
2026-08-26 13:16:18 -0700

    Add stateless tanh module

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -21,7 +21,7 @@ mod shape;
 mod softmax;
 mod state;
 
-pub use activation::{GELU, GeluApproximation, ReLU, SiLU, Sigmoid};
+pub use activation::{GELU, GeluApproximation, ReLU, SiLU, Sigmoid, Tanh};
 pub use conv::{Conv1d, Conv1dOptions, Conv2d, ConvTranspose1d, ConvTranspose2d};
 pub use embedding::Embedding;
 pub use linear::Linear;
