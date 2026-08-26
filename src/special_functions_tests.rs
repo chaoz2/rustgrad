e0501bebf38d08d23bc95c920c0076752e671e74
Chao Zhao <chaoz.db@icloud.com>
2026-08-25 23:34:10 -0700

    Test ReLU6 negative zero composition

--- src/special_functions_tests.rs
+++ src/special_functions_tests.rs
@@ -477,7 +477,7 @@ fn relu6_matches_tinygrad_relu_difference() {
     assert!(values[1].is_nan());
     assert_eq!(values[2], 0.0);
     assert_eq!(values[3], 0.0);
-    assert!(values[3].is_sign_positive());
+    assert!(values[3].is_sign_negative());
     assert_eq!(values[4], 0.0);
     assert!(values[4].is_sign_positive());
     let operations = graph
