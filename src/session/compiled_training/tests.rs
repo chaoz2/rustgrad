9b9a0c2fe3553b83dfbd7cca7339082fa4052fcd
Chao Zhao <chaoz.db@icloud.com>
2026-09-23 05:10:45 -0700

    refactor: isolate compiled optimizer lowering

--- src/session/compiled_training/tests.rs
+++ src/session/compiled_training/tests.rs
@@ -1,5 +1,7 @@
 use super::module_adamw_checkpoint::decode_module_adamw_checkpoint;
-use super::observation::{AdamWObservation, CompiledTrainingObservationValue};
+use super::observation::{
+    AdamWObservation, CompiledTrainingObservationNode, CompiledTrainingObservationValue,
+};
 use super::*;
 use crate::nn::{ParameterSnapshot, StateKind};
 use crate::{Backend, CpuBackend, LossOptions, Op, Parameter, cross_entropy};
