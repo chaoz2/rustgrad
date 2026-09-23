1383011faa84d561f6ba7f5991cb54f08449d03f
Chao Zhao <chaoz.db@icloud.com>
2026-09-23 00:46:29 -0700

    refactor: isolate compiled module state

--- src/session/compiled_training/tests.rs
+++ src/session/compiled_training/tests.rs
@@ -1,5 +1,6 @@
 use super::module_adamw_checkpoint::decode_module_adamw_checkpoint;
 use super::*;
+use crate::nn::{ParameterSnapshot, StateKind};
 use crate::{Backend, CpuBackend, LossOptions, Op, Parameter, cross_entropy};
 use std::{
     cell::Cell,
